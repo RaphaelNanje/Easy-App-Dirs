7c3d73cdead144441b69a7648a26f7389c25d04d
raphael <rtnanje@gmail.com>
2019-10-07 07:19:33 -0400

    Add override option
    Give user the option of choosing whether to override preexisting file_names.
    
    Also adding recursive option to directory_load()

--- easyappdirs/core.py
+++ easyappdirs/core.py
@@ -1,16 +1,22 @@
 import json
-from os import mkdir, makedirs, walk
-from os.path import join, exists, splitext
+from os import mkdir, makedirs, walk, listdir
+from os.path import join, exists, splitext, isfile
 
 import appdirs
 from logzero import logger
 
+from easyappdirs.exceptions import NameAlreadyRegisteredError
+
 
 class EasyAppDirs(appdirs.AppDirs):
-    def __init__(self, app_name, app_author, version=None, log_name="logs.log"):
+    def __init__(self, app_name, app_author, version=None, log_name="logs.log", override=False):
+        """
+        :param override: Set this to True to override paths of names that have already been registered
+        """
         super().__init__(app_name, appauthor=app_author, version=version)
         self.file_paths = {}
         self.log_file_name = log_name
+        self.override = False
 
         if not exists(self.user_data_dir):
             mkdir(self.user_data_dir)
@@ -22,6 +28,17 @@ class EasyAppDirs(appdirs.AppDirs):
             mkdir(self.user_log_dir)
 
     def register_file(self, file_name: str, path: str, short_name: str = None):
+
+        if file_name in self.file_paths:
+            if join(path, file_name) == self.file_paths[file_name]:
+                return self.file_paths[file_name]
+            elif not self.override and self.file_paths[file_name] != join(path, file_name):
+                raise NameAlreadyRegisteredError(
+                    f"Override is set to {self.override} and '{file_name}' has already been registered.", file_name,
+                    path)
+            else:
+                logger.info(
+                    f"'{file_name}' already exists and is pointing to '{self.file_paths[file_name]}'... Overriding.")
         if not exists(path):
             makedirs(path, exist_ok=True)
         path = join(path, file_name)
@@ -49,11 +66,20 @@ class EasyAppDirs(appdirs.AppDirs):
         """
         Load and register all files within the specified directory
         """
-        for root, dirs, files in walk(path, topdown=False):
-            for name in files:
-                file_name = name
-                short_name = splitext(name)[0] if splitext(name)[0] != file_name else None
-                self.register_file(file_name, root, short_name)
+        if not recursive:
+            files = [f for f in listdir(path) if isfile(join(path, f))]
+            for file in files:
+                self.register_file(file, path, splitext(file)[0] if splitext(file)[0] != file else None)
+            return files
+        else:
+            files_list = []
+            for root, dirs, files in walk(path, topdown=True):
+                for name in files:
+                    file_name = name
+                    short_name = splitext(name)[0] if splitext(name)[0] != file_name else None
+                    self.register_file(file_name, root, short_name)
+                    files_list.append(file_name)
+            return files_list
 
     def load(self, name: str) -> dict:
         with open(self.get_path(name), "r") as f:
