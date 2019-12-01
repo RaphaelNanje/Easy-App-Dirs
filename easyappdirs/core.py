e3facd2780ecd55877f2d1f16904bff7713a2dc1
raphael <rtnanje@gmail.com>
2019-12-01 02:01:23 -0500

    change load() defaults

--- easyappdirs/core.py
+++ easyappdirs/core.py
@@ -84,7 +84,7 @@ class EasyAppDirs(appdirs.AppDirs):
                     files_list.append(file_name)
             return files_list
 
-    def load(self, name: str, split=False, strip=True):
+    def load(self, name: str, split=True, strip=True):
         """
         :param split: readlines() instead of read() if True
         :param strip: call strip() on each line
