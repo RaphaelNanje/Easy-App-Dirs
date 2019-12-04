ee52f3fa39f5cb749c57db4e002723886fc6fe19
raphael <rtnanje@gmail.com>
2019-12-04 01:53:40 -0500

    fix bug where nonstring iterables would not save on save()

--- easyappdirs/core.py
+++ easyappdirs/core.py
@@ -129,7 +129,7 @@ class EasyAppDirs(appdirs.AppDirs):
             if not isinstance(data, Iterable):
                 f.write(data)
             else:
-                f.write('\n'.join(data))
+                f.write('\n'.join(str(data)))
 
     def json_save(self, name: str, data, default=None, **kwargs):
         self.file_types[name] = 'json'
